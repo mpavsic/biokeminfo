1ce0970fcb9cde79d1a31a68743cc5d79900a49f
Miha Pavšič <miha.pavsic@icloud.com>
2022-02-14 15:40:16 +0100

    changed vaje 2021/2022

--- biokeminfo/_build/jupyter_execute/vaje/matrika_zamenjav.py
+++ biokeminfo/_build/jupyter_execute/vaje/matrika_zamenjav.py
@@ -1,14 +1,20 @@
-# VAJA: Izračun matrike zamenjav (Python)
+#!/usr/bin/env python
+# coding: utf-8
 
-Za izračun poravnave potrebujemo ocenjevalno shemo, ki zraven vrednosti za odprtje in razširitev vrzeli vsebuje še vrednosti za enakost in neenakost. Najpreprostejši primer za slednje je, na primer, `1` za identičnost in `0` za različnost, a to ne odseva relativne mutabilnosti posameznih aminokislinskih ali nukleotidnih ostankov. Za ta namen so bile razvite t. i. matrike zamenjav ([*substitution matrices*](https://en.wikipedia.org/wiki/Substitution_matrix)), ki so sáme izračunane na  osnovi poravnav.
+# # VAJA: Izračun matrike zamenjav (Python)
+# 
+# Za izračun poravnave potrebujemo ocenjevalno shemo, ki zraven vrednosti za odprtje in razširitev vrzeli vsebuje še vrednosti za enakost in neenakost. Najpreprostejši primer za slednje je, na primer, `1` za identičnost in `0` za različnost, a to ne odseva relativne mutabilnosti posameznih aminokislinskih ali nukleotidnih ostankov. Za ta namen so bile razvite t. i. matrike zamenjav ([*substitution matrices*](https://en.wikipedia.org/wiki/Substitution_matrix)), ki so sáme izračunane na  osnovi poravnav.
+# 
+# ## Namen vaje
+# 
+# Namen vaje je na osnovi enostavne poravnave dveh zaporedij izračunati matriko zamenjav za nukleotidna zaporedja. Uporabili bomo kar zgled iz BioPythona, uporabili pa bomo še knjižnjico `numpy`.
+# 
+# ## Primer kode
+# 
+# V spodnjem primeru sta uporabljeni dve res kratki zaporedji in sicer:
 
-## Namen vaje
+# In[1]:
 
-Namen vaje je na osnovi enostavne poravnave dveh zaporedij izračunati matriko zamenjav za nukleotidna zaporedja. Uporabili bomo kar zgled iz BioPythona, uporabili pa bomo še knjižnjico `numpy`.
-
-## Primer kode
-
-V spodnjem primeru sta uporabljeni dve res kratki zaporedji in sicer:
 
 # preberemo zaporedji
 from Bio import SeqIO
@@ -19,9 +25,13 @@ sequence2 = SeqIO.read('vhod/matrika_zamenjav_seq2.fasta', 'fasta')
 print('Zaporedje 1: %s' % sequence1.seq)
 print('Zaporedje 2: %s' % sequence2.seq)
 
-Vidimo, da sta zaporedji enako dolgi, razlik med njima je malo.
 
-Za izdelavo matrike ju moramo najprej poravnati, pri tem pa bomo naredili lokalno poravnavo z uporabo nekih namišljenih vrednosti za identičnost, neidentičnost ter vrzeli:
+# Vidimo, da sta zaporedji enako dolgi, razlik med njima je malo.
+# 
+# Za izdelavo matrike ju moramo najprej poravnati, pri tem pa bomo naredili lokalno poravnavo z uporabo nekih namišljenih vrednosti za identičnost, neidentičnost ter vrzeli:
+
+# In[2]:
+
 
 from Bio.Align import PairwiseAligner
 aligner = PairwiseAligner()
@@ -44,7 +54,11 @@ alignment = alignments[0]
 # izpišemo poravnavo
 print(alignment)
 
-Za štetje si najprej definiramo matriko, ki vsebuje posamezne ostanke, v našem primeru gre za nukleotide:
+
+# Za štetje si najprej definiramo matriko, ki vsebuje posamezne ostanke, v našem primeru gre za nukleotide:
+
+# In[3]:
+
 
 # definiramo dvodimenzionalno matriko/array (dims=2)
 from Bio.Align.substitution_matrices import Array
@@ -53,7 +67,11 @@ frequency = Array('ACGT', dims=2)
 # izpišemo matriko - zdaj je še prazna
 print(frequency)
 
-Ko imamo pripravljeno poravnavo lahko preštejemo pare ostankov (v tem primeru nukleotidov) in izpišemo njihovo frekvenco v obliki matrike.
+
+# Ko imamo pripravljeno poravnavo lahko preštejemo pare ostankov (v tem primeru nukleotidov) in izpišemo njihovo frekvenco v obliki matrike.
+
+# In[4]:
+
 
 for (start1, end1), (start2, end2) in zip(*alignment.aligned):
     seq1 = sequence1[start1:end1]
@@ -62,9 +80,13 @@ for (start1, end1), (start2, end2) in zip(*alignment.aligned):
         frequency[c1, c2] += 1
 print(frequency)
 
-Vidimo, da imamo 7 parov A-A, 1 par A-C ter 5 parov C-A ... Skupno število parov je 27, kar je v našem primeru enako dolžini poravnave, saj nimamo vrzeli.
 
-V nadaljevanju frekvenco posameznega para delimo s število vseh parov (izvedemo **normalizacijo**), pri tem pa upoštevamo, da sta para A-C ter C-A ekvivalentna (ter analogno za ostale pare). Slednje pomeni, da bomo dobili **simetrično matriko**.
+# Vidimo, da imamo 7 parov A-A, 1 par A-C ter 5 parov C-A ... Skupno število parov je 27, kar je v našem primeru enako dolžini poravnave, saj nimamo vrzeli.
+# 
+# V nadaljevanju frekvenco posameznega para delimo s število vseh parov (izvedemo **normalizacijo**), pri tem pa upoštevamo, da sta para A-C ter C-A ekvivalentna (ter analogno za ostale pare). Slednje pomeni, da bomo dobili **simetrično matriko**.
+
+# In[5]:
+
 
 import numpy
 
@@ -77,29 +99,42 @@ probabilities = (probabilities + probabilities.transpose()) / 2.0
 # izpišemo matriko, na 4 decimalna mesta
 print(format(probabilities, "%.4f"))
 
-Izračunamo verjetnosti ozadja, t. j. verjetnost pojavljanja posameznega ostanka posamično v posamičnem zaporedju. To je dejansko vsota po stolpcih ali po vrsticah - vseeno je, kaj vzamemo, saj je naša matrika simetrična.
+
+# Izračunamo verjetnosti ozadja, t. j. verjetnost pojavljanja posameznega ostanka posamično v posamičnem zaporedju. To je dejansko vsota po stolpcih ali po vrsticah - vseeno je, kaj vzamemo, saj je naša matrika simetrična.
+
+# In[6]:
+
 
 # seštejemo in izpišemo
 background = numpy.sum(probabilities, 0)
 print(format(background, "%.4f"))
 
-Zdaj lahko izračunamo število pričakovanih substitucij, ki je enako kar produktu verjetnosti ozadja s samim seboj.
+
+# Zdaj lahko izračunamo število pričakovanih substitucij, ki je enako kar produktu verjetnosti ozadja s samim seboj.
+
+# In[7]:
+
 
 expected = numpy.dot(background[:,None], background[None, :])
 print(format(expected, "%.4f"))
 
-Končno, matriko zamenjav izračunamo kot logaritem obetov. Obeti so verjetnosti, deljemo z naključno pričakovanimi vrednostmi, vzamemo pa logaritem na osnovi 2.
+
+# Končno, matriko zamenjav izračunamo kot logaritem obetov. Obeti so verjetnosti, deljemo z naključno pričakovanimi vrednostmi, vzamemo pa logaritem na osnovi 2.
+
+# In[8]:
+
 
 oddsratios = probabilities / expected
 scoring_matrix = numpy.log2(oddsratios)
 print(scoring_matrix)
 
-Opazite, da ker nekaterih zamenjav nismo opazili, dobimo ponekod "vrednosti" `-inf`, prav tako smo dobili napako.
 
-## Naloga
+# Opazite, da ker nekaterih zamenjav nismo opazili, dobimo ponekod "vrednosti" `-inf`, prav tako smo dobili napako.
 
-Vaša naloga je, da na osnovi dveh daljših nukleotidnih zaporedjih pripravite matriko zamenjav za nukleotidna zaporedja.
-
-Ustrezna zaporedja pridobite iz zbirke GenBank. Na primer, lahko vzamete genomski zaporedji za gen 16S rRNA za dve različni bakteriji, lahko pa vzamete tudi kakšni daljši ali krajši zaporedji. Pazite le, da bosta zaporedji homologni. Izračunano matriko zamenjav primerjajte z matriko EDNAfull (imenovano tudi DNAfull ali NUC4.4), ki se splošno uporablja pri poravnavah nukleotidnih zaporedij. Prikazana je spodaj.
-
-![EDNAfull matrika zamenjav](slike/ednafull_subs_matrix.png)
+# ## Naloga
+# 
+# Vaša naloga je, da na osnovi dveh daljših nukleotidnih zaporedjih pripravite matriko zamenjav za nukleotidna zaporedja.
+# 
+# Ustrezna zaporedja pridobite iz zbirke GenBank. Na primer, lahko vzamete genomski zaporedji za gen 16S rRNA za dve različni bakteriji, lahko pa vzamete tudi kakšni daljši ali krajši zaporedji. Pazite le, da bosta zaporedji homologni. Izračunano matriko zamenjav primerjajte z matriko EDNAfull (imenovano tudi DNAfull ali NUC4.4), ki se splošno uporablja pri poravnavah nukleotidnih zaporedij. Prikazana je spodaj.
+# 
+# ![EDNAfull matrika zamenjav](slike/ednafull_subs_matrix.png)
